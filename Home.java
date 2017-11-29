29f8b25ad58260dedde12e30ceb8f6886fd4fb48
Justin <jstnwrds55@gmail.com>
2017-11-28 17:54:29 -0700

    finished the pane example adding in the home file that acts as a class for the panefun file

--- Home.java
+++ Home.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Home {
+
+    private JFrame homeScreen;
+    private JLabel lblName;
+    private JTextField txtName;
+    private JButton okBtn;
+
+    private MyButtonListener listen1 = new MyButtonListener();
+
+    public Home () {
+        homeScreen = new JFrame ();
+
+        homeScreen.setSize(400,200);
+        homeScreen.setTitle("GUI Fun!");
+        homeScreen.setDefaultCloseOperation(homeScreen.EXIT_ON_CLOSE);
+        homeScreen.setLayout(new FlowLayout());
+
+        lblName = new JLabel("Enter your name:");
+        txtName = new JTextField(20);
+        okBtn = new JButton("OK");
+
+        homeScreen.add(lblName);
+        homeScreen.add(txtName);
+        homeScreen.add(okBtn);
+
+        okBtn.addActionListener(listen1);
+        txtName.addActionListener(listen1);
+
+        homeScreen.setVisible(true);
+    }
+
+    private class MyButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            txtName.setBackground(Color.blue);
+
+            homeScreen.setVisible(true);
+        }
+
+    }
+
 }
--- PaneFun.java
+++ PaneFun.java
@@ -1,27 +1,6 @@
-import javax.swing.*;
-import java.awt.*;
-
 public class PaneFun {
     public static void main(String[] args) {
-
-        JFrame homeScreen = new JFrame();
-
-        homeScreen.setSize(400, 200);
-        homeScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        homeScreen.setTitle("New Frame");
-        homeScreen.setLayout(new FlowLayout());
-
-
-        JLabel name = new JLabel("Enter your name: ");
-        JTextField txtName = new JTextField(20);
-        JButton okBtn = new JButton("OK");
-
-        homeScreen.add(name);
-        homeScreen.add(txtName);
-        homeScreen.add(okBtn);
-
-
-        homeScreen.setVisible(true);
+        Home h = new Home();
 
     }
 }
